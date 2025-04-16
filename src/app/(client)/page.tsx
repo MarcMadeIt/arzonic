9479559f12816766c1671725a93156fb5d4f6e5c
Xyrel Zukira <sebastian@birdie.codes>
2025-04-16 10:48:48 +0200

    Added scroll progress bar

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,6 +1,8 @@
 import Hero from "@/components/client/home/Hero";
 import Prices from "@/components/client/home/Prices";
 import Process from "@/components/client/home/Process";
+import NavProcess from "@/components/client/home/NavProcess";
+import Preview from "@/components/client/home/Preview";
 import React from "react";
 
 const Home = () => {
@@ -12,9 +14,14 @@ const Home = () => {
       <section className="h-[800px]">
         <Prices />
       </section>
-
       <section className="h-[200px]">
+        <NavProcess />
+      </section>
+      <section id="Process" className="h-[2000px]">
         <Process />
+      </section>
+      <section className="h-[2000px]">
+        <Preview />
       </section>
     </>
   );
